9073ace2a223b5883c92417d0d10553d1c92a99d
ayshacr123 <ayshaayu17@gmail.com>
2023-05-20 17:35:32 +0530

    first commit

---
+++ src/test/java/com/UST/demoJenkins/DemoJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UST.demoJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
